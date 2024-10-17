b6d473451ee1e38f0d4d32392d577d9158c17b85
VWNET\mura <mura@vwnet.jp>
2024-10-17 17:26:41 +0900

    1st commit

---
+++ Readme.txt
@@ -0,0 +1,7 @@
+﻿# 以下コマンドでインストール
+
+$ModuleName = "RemoveAttribute"
+$GitHubName = "MuraAtVwnet"
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/$GitHubName/$ModuleName/master/OnlineInstall.ps1 -OutFile ~/OnlineInstall.ps1
+& ~/OnlineInstall.ps1
+
---
+++ uninstall.ps1
@@ -0,0 +1,27 @@
+﻿# モジュール名
+$ModuleName = "RemoveAttribute"
+
+# モジュール Path
+$ModulePath = Join-Path (Split-Path $PROFILE -Parent) "Modules"
+
+# モジュールが配置されている Path
+$RemovePath = Join-Path $ModulePath $ModuleName
+
+# ディレクトリ削除
+if( Test-Path $RemovePath ){
+	Remove-Item $RemovePath -Force -Recurse
+}
+
+# オンラインインインストール対応
+$UnInstaller = "UnInstall" + $ModuleName + ".ps1"
+if( Test-Path ~/$UnInstaller ){
+	Remove-Item ~/$UnInstaller
+}
+
+# バージョン管理ファイル削除
+$Vertion = "Vertion" + $ModuleName + ".txt"
+if( Test-Path ~/$Vertion ){
+	Remove-Item ~/$Vertion
+}
+
+
